fa06e9bd093fb92b7abf7e3421a0fa0f5fefe85d
mksyahmi24 <mkhairulsyahmi24@gmail.com>
2024-12-13 20:01:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,9 +90,8 @@ if uploaded_file is not None:
 
     # Streamlit input interface for parameters
     with st.form("my_form"):
-        TARGET = st.text_input("Enter your name", "keisyahhh")  # Default name
-        CO_R = st.slider("Crossover Rate", 0.0, 0.95, 0.8, 0.01)  # Adjusted slider
-        MUT_R = st.slider("Mutation Rate", 0.01, 0.05, 0.02, 0.01)  # Adjusted slider
+        CO_R = st.number_input("Crossover Rate", min_value=0.0, max_value=0.95, value=0.8, step=0.01)  # Manual input for crossover rate
+        MUT_R = st.number_input("Mutation Rate", min_value=0.01, max_value=0.05, value=0.02, step=0.01)  # Manual input for mutation rate
         calculate = st.form_submit_button("Calculate")
 
         if calculate:
